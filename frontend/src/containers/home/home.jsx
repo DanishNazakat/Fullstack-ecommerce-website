b922d9e58caa6f0fb53e52d4135eead6f214232a
DanishNazakat <danishnazakat636@gmail.com>
2026-02-24 15:42:07 +0500

    get Products

--- frontend/src/containers/home/home.jsx
+++ frontend/src/containers/home/home.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
-
+import { useState , useEffect } from 'react'
+import {getProduct} from "../../services/auth/getProduct"
 function Home() {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getProduct();
+
+        console.log(response)
+
+
+        setProducts((prevProducts) => [
+          ...prevProducts,
+          ...response
+        ])
+        console.log(products)
+        console.log(response);
+      } catch (err) {
+        console.log(`Products not Found ${err.message}`)
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <div>home</div>
   )
