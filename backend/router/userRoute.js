08fcd60925484ed1b6a426919810b5ef42930b7c
DanishNazakat <danishnazakat636@gmail.com>
2026-02-27 23:49:39 +0500

    role base

--- backend/router/userRoute.js
+++ backend/router/userRoute.js
@@ -1,3 +1,17 @@
 const express = require("express");
 
-c
+const router = express.Router();
+
+// router.post('/superAdmin',superAdmin);
+router.get('/admin', (req, res) => {
+    res.json({
+        message: "Welocome Admin",
+    })
+})
+router.get('/user', (req, res) => {
+    res.json({
+        message: "Welocome User",
+    })
+})
+
+module.exports = router;
